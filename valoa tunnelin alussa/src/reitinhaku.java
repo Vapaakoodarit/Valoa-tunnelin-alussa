d55578458f90b47bdeca13c6fa030723877bab91
Nuurori <nestori.lehtonen@gmail.com>
2014-11-01 22:47:43 +0200

    reitinhakualgoritmi luolaston satunnaisgenerointia varten

---
+++ valoa tunnelin alussa/src/reitinhaku.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Nestori
+ *
+ */
+public class reitinhaku {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
